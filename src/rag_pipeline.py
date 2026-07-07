d04eb254401e45e9da8d162e613f5013c3fbda36
Kashan <kashanaliafzal@outlook.com>
2026-07-07 12:21:25 +0500

    API key setting

--- src/rag_pipeline.py
+++ src/rag_pipeline.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
